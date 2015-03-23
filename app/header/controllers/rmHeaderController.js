1d8acc1410b03d761763d8a4b5982183128913f0
Rodrigo Morais <morais.rm@gmail.com>
2015-03-23 12:05:29 -0300

    Cleaning the garbage from the files.

--- app/header/controllers/rmHeaderController.js
+++ app/header/controllers/rmHeaderController.js
@@ -1,35 +1,13 @@
 export default function rmHeaderController($scope){
-        $scope.logged = false;
+    $scope.logged = false;
 
-        $scope.connect = function(token, expiresIn){
-            $scope.$apply(function() {
-                $scope.logged = true;
-            });
-        };
-
-        $scope.disconnect = function(){
-            $scope.logged = false;
-        };
+    $scope.connect = function(token, expiresIn){
+        $scope.$apply(function() {
+            $scope.logged = true;
+        });
     };
 
-/*(function(){
-    'use strict';
-
-    app.controller('rmHeaderController',
-    ['$scope', rmHeaderController]);
-
-    function rmHeaderController($scope){
+    $scope.disconnect = function(){
         $scope.logged = false;
-
-        $scope.connect = function(token, expiresIn){
-            $scope.$apply(function() {
-                $scope.logged = true;
-            });
-        };
-
-        $scope.disconnect = function(){
-            $scope.logged = false;
-        };
-    }
-
-})();*/
+    };
+};
--- app/header/directives/rmHeaderDirective.js
+++ app/header/directives/rmHeaderDirective.js
@@ -3,125 +3,59 @@ import rmHeaderController from "header/controllers/rmHeaderController";
 
 export default function rmHeaderDirective($window, rmConsumer, OauthAccess) {
 
-        var _requestAuthorization = function() {
-            var width = 500,
-                height = 350,
-                top = (screen.height - height)/2,
-                left = (screen.width - width)/2;
-            $window.open(
-                rmConsumer.authorize_uri,
-                "Meetup",
-                ["height=", height, ",width=", width,
-                 ",top=", top, ",left=", left].join(''));    
-        };
-
-        var _isAuthorized = function(){
-            var frag = window.location.hash;
-            if(frag) {
-                var fp = frag.substring(1).split('&'),
-                    i = fp.length,
-                    params = {},
-                    re = /(\S+)=(\S+)/,
-                    inject = function(pair) {
-                        if(re.test(pair)) {
-                            var kv = re.exec(pair).splice(1, 2);
-                            params[kv[0]] = kv[1];
-                        }
-                    };
-
-                    while(i--){
-                        inject(fp[i]);
-                    }
-
-                    if(params.access_token) {
-                        $window.close();
-                        $window
-                            .opener
-                            .onMeetupAuth(params.access_token, params.expires_in);
-                    } else if(params.error) {
-                        $window.close();
-                        $window.opener.onMeetupDenial(params.error);
-                    }
-                }
-        };
-
-        var html = 'app/header/templates/header.html';
-
-        return {
-            restrict: 'E',
-            templateUrl: html,
-            css: 'app/header/css/header.css',
-            replace: true,
-            controller: rmHeaderController,
-            link: function (scope, element, attrs, controller) {
-                
-            }
-        };
-
+    var _requestAuthorization = function() {
+        var width = 500,
+            height = 350,
+            top = (screen.height - height)/2,
+            left = (screen.width - width)/2;
+        $window.open(
+            rmConsumer.authorize_uri,
+            "Meetup",
+            ["height=", height, ",width=", width,
+             ",top=", top, ",left=", left].join(''));    
     };
 
-/*(function() {
-    'use strict';
-    
-    app.directive('rmHeader',
-    ['$window', 'rmConsumer', 'OauthAccess',rmHeaderDirective]);
-
-    function rmHeaderDirective($window, rmConsumer, OauthAccess) {
-
-        var _requestAuthorization = function() {
-            var width = 500,
-                height = 350,
-                top = (screen.height - height)/2,
-                left = (screen.width - width)/2;
-            $window.open(
-                rmConsumer.authorize_uri,
-                "Meetup",
-                ["height=", height, ",width=", width,
-                 ",top=", top, ",left=", left].join(''));    
-        };
-
-        var _isAuthorized = function(){
-            var frag = window.location.hash;
-            if(frag) {
-                var fp = frag.substring(1).split('&'),
-                    i = fp.length,
-                    params = {},
-                    re = /(\S+)=(\S+)/,
-                    inject = function(pair) {
-                        if(re.test(pair)) {
-                            var kv = re.exec(pair).splice(1, 2);
-                            params[kv[0]] = kv[1];
-                        }
-                    };
-
-                    while(i--){
-                        inject(fp[i]);
+    var _isAuthorized = function(){
+        var frag = window.location.hash;
+        if(frag) {
+            var fp = frag.substring(1).split('&'),
+                i = fp.length,
+                params = {},
+                re = /(\S+)=(\S+)/,
+                inject = function(pair) {
+                    if(re.test(pair)) {
+                        var kv = re.exec(pair).splice(1, 2);
+                        params[kv[0]] = kv[1];
                     }
+                };
 
-                    if(params.access_token) {
-                        $window.close();
-                        $window
-                            .opener
-                            .onMeetupAuth(params.access_token, params.expires_in);
-                    } else if(params.error) {
-                        $window.close();
-                        $window.opener.onMeetupDenial(params.error);
-                    }
+                while(i--){
+                    inject(fp[i]);
                 }
-        };
 
-        var html = 'app/header/templates/header.html';
-
-        return {
-            restrict: 'E',
-            templateUrl: html,
-            css: 'app/header/css/header.css',
-            replace: true,
-            controller: 'rmHeaderController',
-            link: function (scope, element, attrs, controller) {
-                
+                if(params.access_token) {
+                    $window.close();
+                    $window
+                        .opener
+                        .onMeetupAuth(params.access_token, params.expires_in);
+                } else if(params.error) {
+                    $window.close();
+                    $window.opener.onMeetupDenial(params.error);
+                }
             }
-        };
+    };
 
-    }
-})();*/
+    var html = 'app/header/templates/header.html';
+
+    return {
+        restrict: 'E',
+        templateUrl: html,
+        css: 'app/header/css/header.css',
+        replace: true,
+        controller: rmHeaderController,
+        link: function (scope, element, attrs, controller) {
+            
+        }
+    };
+
+};
